d6b09e1b5a02d31b03de57a553000152b593268e
junho85 <junho85@gmail.com>
2017-01-15 21:28:02 +0900

    ~p57

---
+++ src/test/Ex_3_5.scala
@@ -0,0 +1,11 @@
+package test
+
+object Ex_3_5 {
+  def main(args: Array[String]): Unit = {
+    var a = 0
+    while (a < 20) {
+      print(a + ",")
+      a = a + 1 // there is no a++
+    }
+  }
+}
---
+++ src/test/Ex_3_9.scala
@@ -0,0 +1,19 @@
+package test
+
+object Ex_3_9 {
+  def main(args: Array[String]): Unit = {
+    for (i <- 1 to 10 if i % 2 == 0) {
+      println(i)
+    }
+
+    // Five ways to create a Scala List
+    // http://alvinalexander.com/scala/how-create-scala-list-range-fill-tabulate-constructors
+//    val list = List(1,2,3,4,5,6,7,8,9,10)
+//    val list = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: 7 :: 8 :: 9 :: 10 :: Nil // Lisp Style
+//    val list = List.range(1, 10) // 1 to 9
+    val list = List.range(0, 10, 2) // 0,2,4,6,8
+    for ((num, index) <- list.zipWithIndex) {
+      println(num + ":" + index)
+    }
+  }
+}
